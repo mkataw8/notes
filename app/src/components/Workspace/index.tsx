3529446a74b3d60805ea08be02392e71b4121a34
mkataw8 <mkataw8@gmail.com>
2023-09-25 14:55:07 -0400

    first commit. Creating front end

---
+++ app/src/components/Workspace/index.tsx
@@ -0,0 +1,14 @@
+export default function Workspace() {
+  return (
+    <div className="bg-slate-400 w-full h-screen">
+      <div className="flex justify-center space-x-4 pt-10 ">
+        <div className="bg-white w-80 h-64">These are my notes</div>
+        <div className="bg-white w-80 h-64"></div>
+        <div className="bg-white w-80 h-64"></div>
+      </div>
+      <div className="flex justify-center">
+        <p className=" pt-10">add more...</p>
+      </div>
+    </div>
+  );
+}
